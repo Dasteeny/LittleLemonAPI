adc6a73f2de31fe5b09dcf25f74a0181d679f2ff
Dasteeny <dastan.zhabagin@gmail.com>
2023-01-19 16:19:26 +0600

    Add djoser

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "LittleLemonAPI",
+    "rest_framework",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DJOSER = {"USER_ID_FIELD": "username"}
