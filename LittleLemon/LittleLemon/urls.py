03c0dc96477f63e202b92dcb758eb2017157f22f
Dasteeny <dastan.zhabagin@gmail.com>
2023-01-20 12:12:09 +0600

    Setup djoser and urls

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
+    path("api/", include("LittleLemonAPI.urls")),
+    path("api/", include("djoser.urls")),
+    path("", include("djoser.urls.authtoken")),
 ]
---
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+urlpatterns = [
+    # path("menu-items",),
+    # path("menu-items/<int:pk>",),
+    # path("groups/manager/users", ),
+    # path("groups/manager/users/<int:pk>", ),
+    # path("groups/delivery-crew/users", ),
+    # path("groups/delivery-crew/users/<int:pk>", ),
+    # path("cart/menu-items", ),
+    # path("cart/orders", ),
+    # path("cart/orders/<int:pk>", ),
+]
